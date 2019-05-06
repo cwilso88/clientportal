e3661ade4da23d41825b719929beb3c4c30ab6ce
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-06 11:19:01 -0400

    Created if else statement for the Clients.js component to render either the clients list or Loading... if the list has not come in yet from firebas

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react'
 
 class Clients extends Component {
   render() {
-    return (
-      <div>
-        <h1>Clients</h1>
-      </div>
-    )
+    const clients = [{
+      id: '3414',
+      firstName: 'Amanda',
+      lastName: 'Murphy',
+      email: 'amandam@gmail.com',
+      phone: '770-765-0909',
+      balance: '150'
+    }];
+
+    if(clients){
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+                <h2 className=""><i className="fas fa-users"></i></h2>
+            </div>
+            <div className="col-md-6">
+
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <h1>Loading..</h1>
+    }
   }
 }
 
