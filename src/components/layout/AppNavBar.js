e9aeb70b1929f5ab51481e6ab2662c11dbac1397
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-06 09:55:36 -0400

    changed the structure of the code for the button and span to be included inside the div with the LINK

--- src/components/layout/AppNavBar.js
+++ src/components/layout/AppNavBar.js
@@ -7,9 +7,17 @@ class AppNavBar extends Component {
       <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
           <div className="container">
             <Link to="/" className="navbar-brand">
-                
+                ClientPanel
             </Link>
-          </div>
+            <button 
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarMain">
+                <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarMain"></div>
+        </div>
       </nav>
     )
   }
