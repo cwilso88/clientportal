f97717785cc4c0761450e11b3b8bd27eedc5c1bf
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-06 11:08:10 -0400

    Added the New to the button on the sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <Link to="/client/add" className="btn btn-success btn-block">
-        <i className="fas fa-plus"></i>
+        <i className="fas fa-plus"></i> New
     </Link>
   )
 }
