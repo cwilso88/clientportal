f7d03cd638a769d8926e71c31ec29a429649abae
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-06 11:05:08 -0400

    included the Clients and Sidebar components into the Dashboard component

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -7,10 +7,12 @@ export default () => {
   return (
     <div className="row">
       <div className="col-md-10">
-
+        <Clients />
+      </div>
+      <div className="col-md-2">
+        <Sidebar />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard;
