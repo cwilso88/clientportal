eb56e07920f090310e0b0d214427025167b6d415
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-07 10:24:31 -0400

    added reactReduxFirebase enhancer for making the store

--- src/store.js
+++ src/store.js
@@ -16,7 +16,24 @@ const firebaseConfig = {
     storageBucket: "reactclientpanelproject.appspot.com",
     messagingSenderId: "752016148981",
     appId: "1:752016148981:web:0dc7e31298d19d6b"
-}
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  }
+
+  // Init firebase instance
+  firebase.initializeApp(firebaseConfig);
+  // Init firestore
+  const firestore = firebase.firestore();
+
+  // Add reactReduxFirebase enhancer when making store creator
+  const createStoreWithFirebase = compose(
+    reactReduxFirebase(firebase, rrfConfig), // firebase instance as first argument
+    // reduxFirestore(firebase) // <- needed if using firestore
+ )(createStore)
 
 
 
