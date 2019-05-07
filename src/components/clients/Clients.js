114ddb8187e342ed02d127d918545696a11942cf
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-07 09:41:13 -0400

    Createing the table for client information once it's brought in from firebase

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Clients extends Component {
   render() {
@@ -25,6 +26,30 @@ class Clients extends Component {
 
             </div>
           </div>
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>{client.firstName} {client.lastName}</td>
+                  <td>{client.email}</td>
+                  <td>{client.balance}</td>
+                  <td>
+                    <Link to={`/client/${client.id}`} className="btn btn-secondary btn-sm">
+                      <i className="fas fa-arrow-circle-right">Details</i>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       );
     } else {
