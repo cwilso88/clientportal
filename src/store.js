cda247c540f5c49352e4c8a4931b4356b3274914
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-07 10:15:38 -0400

    Added config code from firebase to the store.js

--- src/store.js
+++ src/store.js
@@ -8,5 +8,15 @@ import { reduxFirestore, firestoreReducer} from 'redux-firestore';
 
 //@todo
 
+const firebaseConfig = {
+    apiKey: "AIzaSyACiOa4JYb6oq9UNvfQhAoC_-3mP5JdmQQ",
+    authDomain: "reactclientpanelproject.firebaseapp.com",
+    databaseURL: "https://reactclientpanelproject.firebaseio.com",
+    projectId: "reactclientpanelproject",
+    storageBucket: "reactclientpanelproject.appspot.com",
+    messagingSenderId: "752016148981",
+    appId: "1:752016148981:web:0dc7e31298d19d6b"
+}
+
 
 
