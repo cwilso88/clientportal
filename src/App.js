a7da2613e2e76f8c516b3cff7484b0d86a8d13af
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-08 11:07:55 -0400

    Created the AddClient coponent and added the Route to /client/add to link with the NEW button on main page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import store from './store';
 import AppNavbar from './components/layout/AppNavBar';
 import Dashboard from './components/layout/Dashboard';
+import AddClient from './components/clients/AddClient';
+
+
 import './App.css';
 
 function App() {
@@ -15,6 +18,7 @@ function App() {
       <div className="container">
         <Switch>
           <Route exact path="/" component={Dashboard}></Route>
+          <Route exact path="/client/add" component={AddClient}></Route>
           
         </Switch>
       </div>
