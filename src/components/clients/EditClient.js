188ecef84c7bcbc950bf4324c16868263c8d955a
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-09 16:05:20 -0400

    added markup to EditClient for the edit form

--- src/components/clients/EditClient.js
+++ src/components/clients/EditClient.js
@@ -9,12 +9,107 @@ import Spinner from '../layout/Spinner';
 
 class EditClient extends Component {
   render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
+      const { client } = this.props;
+
+
+    if(client) {
+        return (
+        <div>
+        <div className="row">
+            <div className="col-md-6">
+                <Link to="/" className="btn btn-link">
+                    <i className="fas fa-arrow-circle-left"></i>
+                        Back To Dashboard
+                </Link>
+            </div>
+        </div>
+        
+        <div className="card">
+            <div className="card-header">Add Client</div>
+            <div className="card-body">
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="firstName">First Name</label>
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            name="firstName"
+                            minLength="2"
+                            required
+                            onChange={this.onChange}
+                            value={this.state.firstName}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            name="lastName"
+                            minLength="2"
+                            required
+                            onChange={this.onChange}
+                            value={this.state.lastName}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email" 
+                            className="form-control"
+                            name="email"
+                            onChange={this.onChange}
+                            value={this.state.email}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="phone">Phone</label>
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            name="phone"
+                            minLength="10"
+                            required
+                            onChange={this.onChange}
+                            value={this.state.phone}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="balance">Balance</label>
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            name="balance"
+                            onChange={this.onChange}
+                            value={this.state.balance}
+                        />
+                    </div>
+
+                    <input type="submit" value="Submit" className="btn btn-primary btn-block"/>
+                </form>
+            </div>
+        </div>
       </div>
-    )
+          )
+    } else {
+        return (
+            <Spinner />
+        )
+    }
+    
   }
 }
 
-export default EditClient;
+EditClient.propTypes = {
+    firestore: PropTypes.object.isRequired,
+    client: PropTypes.object
+}
+
+export default compose(
+    firestoreConnect(props => [
+        { collection: 'clients', storeAs: 'client', doc: props.match.params.id }
+    ]), 
+    connect(({ firestore: { ordered } }, props) => ({
+      client: ordered.client && ordered.client[0]
+    }))
+  )(EditClient);
