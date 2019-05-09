c0a8cb1a14627ad46806a225107c5ab5136bde26
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-09 15:39:49 -0400

    Added route for EditClient.js within App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AppNavbar from './components/layout/AppNavBar';
 import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
 import ClientDetails from './components/clients/ClientDetails';
+import EditClient from './components/clients/EditClient';
 
 
 import './App.css';
@@ -21,6 +22,7 @@ function App() {
           <Route exact path="/" component={Dashboard}></Route>
           <Route exact path="/client/add" component={AddClient}></Route>
           <Route exact path="/client/:id" component={ClientDetails}></Route>
+          <Route exact path="/client/eidt/:id" component={EditClient}></Route>
           
         </Switch>
       </div>
