73a311fcfbc53f01f64ff9baae5280190b261030
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-10 08:44:50 -0400

    Starting setup for User Authentication. Created the Login component and required imports to start

---
+++ src/components/auth/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firebaseConnect } from 'react-redux-firebase';
+
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Login;
