992f77a940b1e6935848fedc6ea40c6744542760
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-12 12:00:16 -0400

    Created the payload for setDisableBalanceOnAdd

--- src/actions/settingsActions.js
+++ src/actions/settingsActions.js
@@ -8,12 +8,13 @@ export const setDisableBalanceOnAdd = () => {
     const settings = JSON.parse(localStorage.getItem('settings'));
 
     // Toggle
-    settings.setDisableBalanceOnAdd = !setDisableBalanceOnAdd;
+    settings.disableBalanceOnAdd = !settings.disableBalanceOnAdd;
 
     // Set back to localStorage
     localStorage.setItem('settings', JSON.stringify(settings));
     return {
-        type: DISABLE_BALANCE_ON_ADD
+        type: DISABLE_BALANCE_ON_ADD,
+        payload: settings.disableBalanceOnAdd
     }
 }
 export const setDisableBalanceOnEdit = () => {
