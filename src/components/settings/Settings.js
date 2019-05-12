d393de5549be836341c4fb1a041aa4bf694c73d2
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-12 10:14:49 -0400

    Created the import tags for Settings component

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,4 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-route-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { setAllowRegistration,
+         setDisableBalanceOnAdd,
+         setDisableBalanceOnEdit
+        } from '../../actions/settingsActions';
 
 class Settings extends Component {
   render() {
