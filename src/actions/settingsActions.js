033f6c7cacd8088699d74d2e7c96e265215fedcc
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-12 08:48:51 -0400

    Created the switch statement for DISABLE_BALANCE_ON_ADD to toggle the state to disabled, not disabled

--- src/actions/settingsActions.js
+++ src/actions/settingsActions.js
@@ -1,9 +1,23 @@
 import { DISABLE_BALANCE_ON_ADD, 
         DISABLE_BALANCE_ON_EDIT, 
         ALLOW_REGISTRATION }from '../actions/types';
+import { actionTypes } from 'redux-firestore';
 
 
-export default function settingsActions() {
+const initialState = {
+    disableBalanceOnAdd: true,
+    disableBalanceOnEdit: false,
+    allowRegistration: false
+}
+
+export default function(state = initialState) {
+    switch(action.type) {
+        case DISABLE_BALANCE_ON_ADD:
+        return {
+            ...state,
+            disableBalanceOnAdd: !disableBalanceOnAdd
+        };
+    }
   return (
     <div>
       
