176de358b4c0f0850b5a4c294475b40b5318a2cf
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-12 08:44:19 -0400

    Created the settingsActions file and imported the action types from actions/types file

---
+++ src/actions/settingsActions.js
@@ -0,0 +1,12 @@
+import { DISABLE_BALANCE_ON_ADD, 
+        DISABLE_BALANCE_ON_EDIT, 
+        ALLOW_REGISTRATION }from '../actions/types';
+
+
+export default function settingsActions() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
