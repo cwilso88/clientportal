1e78a0d203db0ec18cd8c6b38fb2ff33dc164203
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-11 22:53:30 -0400

    Added more action types to types.js

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -2,8 +2,8 @@ import React from 'react'
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
- const Alert = (props) => {
-     const { message, messageType } = this.props;
+ const Alert = props => {
+     const { message, messageType } = props;
   return (
     <div className={classnames('alert', {
         'alert-success': messageType === 'success',
