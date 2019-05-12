1198b778dda8852cee725ff190be48615309622d
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-11 21:40:54 -0400

    updated the Reducer to fix small bug

--- src/reducers/notifyReducer.js
+++ src/reducers/notifyReducer.js
@@ -1,19 +1,20 @@
-import { NOTIFY_USER }from '.../actions/types';
+import { NOTIFY_USER } from '../actions/types';
 
 const initialState = {
     message: null,
     messageType: null
 }
 
-export default fuction(state = initialState, action) {
-    switch(action.type) {
-        case NOTIFY_USER:
-            return {
-                ...state,
-                message: action.message,
-                messageType: action.messageTyp
-            };
-            default:
-                return state;
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+      case NOTIFY_USER:
+        return {
+          ...state,     
+          message: action.message,
+          messageType: action.messageType
+        };
+      default:
+        return state;
     }
-}
+  }
