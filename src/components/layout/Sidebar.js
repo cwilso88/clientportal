f114958eee0658c2392b23561ead868056a1ed4b
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-01 23:14:20 -0400

    Changed color to sidebar add new button

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default () => {
   return (
-    <Link to="/client/add" style={{ backgroundColor:"#61A598" }} className="btn btn-block">
+    <Link to="/client/add" style={{ backgroundColor:"#61A598", color:"#3D3331" }} className="btn btn-block">
         <i className="fas fa-plus"></i> New
     </Link>
   )
