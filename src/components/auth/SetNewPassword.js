5a3494eb870eb2f7003a1cc645bb3f95f2ee880f
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-03 22:08:57 -0400

    Set up for custom link to reset password

---
+++ src/components/auth/SetNewPassword.js
@@ -0,0 +1,31 @@
+function handleResetPassword(auth, actionCode, continueUrl, lang) {
+    // Localize the UI to the selected language as determined by the lang
+    // parameter.
+    var accountEmail;
+    // Verify the password reset code is valid.
+    auth.verifyPasswordResetCode(actionCode).then(function(email) {
+      var accountEmail = email;
+  
+      // TODO: Show the reset screen with the user's email and ask the user for
+      // the new password.
+  
+      // Save the new password.
+      auth.confirmPasswordReset(actionCode, newPassword).then(function(resp) {
+        // Password reset has been confirmed and new password updated.
+  
+        // TODO: Display a link back to the app, or sign-in the user directly
+        // if the page belongs to the same domain as the app:
+        // auth.signInWithEmailAndPassword(accountEmail, newPassword);
+  
+        // TODO: If a continue URL is available, display a button which on
+        // click redirects the user back to the app via continueUrl with
+        // additional state determined from that URL's parameters.
+      }).catch(function(error) {
+        // Error occurred during confirmation. The code might have expired or the
+        // password is too weak.
+      });
+    }).catch(function(error) {
+      // Invalid or expired action code. Ask user to try to reset the password
+      // again.
+    });
+  }
